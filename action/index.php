5ff97fce2f40f9a2d65143c9e0db104fc7569bf6
jcolber <jcolber@gmail.com>
2016-05-26 00:24:36 -0600

    Add files via upload

---
+++ action/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include'../templates/head.php';?>
+        
+        
+        <title>Joshua Colbert Photography</title>
+
+    </head>
+    
+    <body>
+        <?php include'../templates/navbar.php';?>
+        
+        <br>
+        <br>
+        <br>
+        Badass page coming soon!
+        <br>
+        <br>
+        
+        <?php include'../templates/footer.php';?>
+    </body>
+</html>
