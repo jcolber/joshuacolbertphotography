8765684b9a873f155c40156c29b46c282e45c539
jcolber <jcolber@gmail.com>
2016-05-26 00:06:55 -0600

    Add files via upload

---
+++ flowers-wildlife/bird-of-paradise.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('../templates/head.php'); ?>
+
+        <title>Bird of Paradise</title>
+
+    </head>
+    
+    <body>
+        
+        <?php include('../templates/navbar.php'); ?>    
+        <div class="full-background col-xs-12">
+            <a class="view col-xs-12">
+                <img src="/images/5x7/Bird-Of-Paridise.jpg" class="img-responsive" alt="Utah Landscape Photography"><br>
+            </a>       
+            <div class="content">
+                <h1>
+                    Bird of Paradise                 
+                </h1>
+                <h2>This was the first time a can recall ever seeing this flower and remembering what it was. Then years later I 
+                    went to Florida and saw them there too. So I'm guessing I've seen more than I know and just didn't realize it.
+                    But it is a pretty cool flower, and I wish we had them in Utah. Good thing the California coast is only 
+                    half a days drive away. This particular flower was in La Jolla, California when we were on our way to check out 
+                    sea lions hanging out in the ocean.
+                </h2><br>
+                    <?php include ('../templates/p1.php'); ?>
+                <?php include ('../templates/pricetable.php'); ?>
+                <?php include ('../templates/p2.php'); ?>
+                <div class="paypal col-xs-12">
+                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                        <input type="hidden" name="cmd" value="_cart">
+                        <input type="hidden" name="business" value="jcolber@gmail.com">
+                        <input type="hidden" name="lc" value="US">
+                       
+                        <!--MAKE SURE TO CHANGE THIS TITLE-->
+                        <input type="hidden" name="item_name" value="Bird of Paradise">
+                        <input type="hidden" name="button_subtype" value="products">
+                        <input type="hidden" name="no_note" value="0">
+                        <input type="hidden" name="cn" value="Add special instructions to the seller:">
+                        <input type="hidden" name="no_shipping" value="2">
+                        <input type="hidden" name="tax_rate" value="6.850">
+                        <input type="hidden" name="add" value="1">
+                        <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHosted">
+                        <?php include ('../templates/paypal.php'); ?>                        
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php include('../templates/footer.php'); ?>
+    </body>
+</html>
---
+++ flowers-wildlife/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include'../templates/head.php';?>
+        <title>Wildlife and Flowers</title>
+
+    </head>
+    
+    <body>
+        
+        <?php include'../templates/navbar.php';?> 
+        
+        
+        <div class="full-background col-xs-12">
+            <a class="landscape col-xs-4" href="/flowers-wildlife/osprey-diving.php">
+                <img src="/images/5x7/Bird-Of-Paridise.jpg" class="img-responsive" alt="Sunset, Antelope Island, Great Salt Lake, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape col-xs-4" href="/flowers-wildlife/cactus.php">
+                <img src="/images/5x7/Cactus.jpg" class="img-responsive" alt="Sundial Peak, Rocky Mountains, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape col-xs-4" href="/flowers-wildlife/colorado-columbine.php">
+                <img src="/images/5x7/Colorado-Columbine.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/fireweed.php">
+                <img src="/images/5x7/Fireweed.jpg" class="img-responsive" alt="Fireweed, Selective color, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/crane-on-beach.php">
+                <img src="/images/5x7/Crane-Bird-On-Beach.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/fall-leaves.php">
+                <img src="/images/5x7/Fall-Oak-Leaves.jpg" class="img-responsive" alt="Skyline Arch, Moab, Arches National Park, Utah Landscape Photography"><br>
+            </a>
+
+
+
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/seagull-sunset.php">
+                <img src="/images/5x7/Seagull-with-Sunset.jpg" class="img-responsive" alt="Sunset, Antelope Island, Great Salt Lake, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/purple-flower.php" >
+                <img src="/images/5x7/Purple-Flower.jpg" class="img-responsive" alt="Mount Timapanogos, Lake, Summit, Josh Colbert, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/flowers-wildlife/buffalo.php">
+                <img src="/images/5x7/Buffalo.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+
+
+
+            <a class="landscape2 col-xs-4" href="/landscapes/sunset.php">
+                <img src="/images/5x7/Sunset.jpg" class="img-responsive" alt="Silouhette Trees, Sunset, Wasatch Mountain, Rocky Mountain Photograph, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <img src="/images/5x7/Wasatch-Night.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/landscapes/lake-blanche.php">
+                <img src="/images/5x7/Sundial-Blanche.jpg" class="img-responsive" alt="Lake Blanche, Sundial Peak, Mount Superior, Monte Cristo, Mountain Lake, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+
+
+
+            <a class="landscape2 col-xs-4" href="/landscapes/downtown-slc.php">
+                <img src="/images/5x7/Downtown-SLC.jpg" class="img-responsive" alt="Downtown Salt Lake City, Salt Lake Temple, Devil Clouds, City Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <img src="/images/5x7/Buffalo.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-4" href="/landscapes/seaweed.php">
+                <img src="/images/5x7/Seaweed.jpg" class="img-responsive" alt="Ocean Trash, Seaweed, California Beach, Beach Photograph"><br>
+            </a>
+        </div>
+        <?php include'../templates/footer.php';?>
+    </body>
+</html>
