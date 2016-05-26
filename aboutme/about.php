68f2f79647a74f9be2fd43524ba20a4e7e8b7189
jcolber <jcolber@gmail.com>
2016-05-26 00:08:27 -0600

    Add files via upload

---
+++ aboutme/about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include'../templates/head.php';?>
+        
+        
+        <title>Utah Landscape Photography</title>
+
+    </head>
+    
+    <body>
+        <?php include'../templates/navbar.php';?>
+        
+        <div class="about">
+            <div class="img-circle hor">
+                <div class="img-circle">
+                    <img src="/images/me.jpg" class="img-responsive" alt="Picture of Josh Colbert">
+                </div>
+            </div>
+        </div>
+       
+    </body>
+</html>
