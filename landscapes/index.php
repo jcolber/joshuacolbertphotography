a5ff7ef68af0d9bf73f528db6e2c31123c6a183e
jcolber <jcolber@gmail.com>
2016-06-12 23:01:15 -0600

    Add files via upload

--- landscapes/index.php
+++ landscapes/index.php
@@ -2,8 +2,19 @@
 <html>
     <head>
         <?php include'../templates/head.php';?>
-        <title>Utah Landscape Photography</title>
+        
 
+        
+        <title>Utah Landscape Photography</title>
+        
+<!--        <script type="text/javascript"> 
+
+            jQuery(document).ready(function(){ 
+                    jQuery("#gallery").unitegallery({
+                    gallery_theme:"justified"}); 
+            }); 
+
+        </script>-->
     </head>
     
     <body>
@@ -11,63 +22,106 @@
         <?php include'../templates/navbar.php';?> 
         
         
+<!--        
+        <div id="gallery">
+
+            <a href="/landscapes/antelope-island-sunset.php">
+                <img alt="Image 1 Title" src="/images/5x7/AntelopeSunset.jpg"
+                 data-image="/images/5x7/AntelopeSunset.jpg"
+                 data-description="Image 1 Description"
+                 href="/landscapes/antelope-island-sunset.php">
+            </a>
+            <a href="/landscapes/sundial-peak.php">
+                <img alt="Image 2 Title" src="/images/5x7/Sundial-Peak-Color.jpg"
+                data-image="/images/5x7/Sundial-Peak-Color.jpg"
+                data-description="Image 1 Description">
+            </a>
+            <a href="/landscapes/antelope-island-sunset.php">
+                <img alt="Image 1 Title" src="/images/5x7/AntelopeSunset.jpg"
+                 data-image="/images/5x7/AntelopeSunset.jpg"
+                 data-description="Image 1 Description"
+                 href="/landscapes/antelope-island-sunset.php">
+            </a>
+            <a href="/landscapes/sundial-peak.php">
+                <img alt="Image 2 Title" src="/images/5x7/Sundial-Peak-Color.jpg"
+                data-image="/images/5x7/Sundial-Peak-Color.jpg"
+                data-description="Image 1 Description">
+            </a>            
+            <a href="/landscapes/antelope-island-sunset.php">
+                <img alt="Image 1 Title" src="/images/5x7/AntelopeSunset.jpg"
+                 data-image="/images/5x7/AntelopeSunset.jpg"
+                 data-description="Image 1 Description"
+                 href="/landscapes/antelope-island-sunset.php">
+            </a>
+            <a href="/landscapes/sundial-peak.php">
+                <img alt="Image 2 Title" src="/images/5x7/Sundial-Peak-Color.jpg"
+                data-image="/images/5x7/Sundial-Peak-Color.jpg"
+                data-description="Image 1 Description">
+            </a>		
+        </div>-->
+        
+        
         <div class="full-background col-xs-12">
-            <a class="landscape col-xs-4" href="/landscapes/antelope-island-sunset.php">
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/antelope-island-sunset.php">
                 <img src="/images/5x7/AntelopeSunset.jpg" class="img-responsive" alt="Sunset, Antelope Island, Great Salt Lake, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape col-xs-4" href="/landscapes/sundial-peak.php">
-                <img src="/images/5x7/Sundial-Peak-Color.jpg" class="img-responsive" alt="Sundial Peak, Rocky Mountains, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/sundial-peak.php">
+                <img src="../images/5x7/Sundial-Peak-Color.jpg" class="img-responsive" alt="Sundial Peak, Rocky Mountains, Wasatch Mountain Range, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape col-xs-4" data-toggle="collapse" href="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                <img src="/images/5x7/Cactus.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" >
+                <img src="../images/5x7/Cactus.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
             </a>
             
-            <a class="landscape2 col-xs-4" href="/landscapes/milky-way.php">
-                <img src="/images/5x7/MilkyWay.jpg" class="img-responsive" alt="Fireweed, Selective color, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/milky-way.php">
+                <img src="../images/5x7/MilkyWay.jpg" class="img-responsive" alt="Fireweed, Selective color, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" href="/landscapes/antelope-island-tree.php">
-                <img src="/images/5x7/Antelope-Island-Tree.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/antelope-island-tree.php">
+                <img src="../images/5x7/Antelope-Island-Tree.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" href="/landscapes/skyline-arch.php">
-                <img src="/images/5x7/Skyline-Arch.jpg" class="img-responsive" alt="Skyline Arch, Moab, Arches National Park, Utah Landscape Photography"><br>
-            </a>
-
-
-
-            <a class="landscape2 col-xs-4" href="/landscapes/wasatch-night.php">
-                <img src="/images/5x7/Wasatch-Night.jpg" class="img-responsive" alt="Fireweed, Selective color, Utah Landscape Photography"><br>
-            </a>
-            <a class="landscape2 col-xs-4" href="/landscapes/timpanogos-from-top.php" >
-                <img src="/images/5x7/Timp-North.jpg" class="img-responsive" alt="Mount Timapanogos, Lake, Summit, Josh Colbert, Utah Landscape Photography"><br>
-            </a>
-            <a class="landscape2 col-xs-4" data-toggle="collapse" href="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                <img src="/images/5x7/Bird-Of-Paridise.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/skyline-arch.php">
+                <img src="../images/5x7/Skyline-Arch.jpg" class="img-responsive" alt="Skyline Arch, Moab, Arches National Park, Utah Landscape Photography"><br>
             </a>
 
 
 
-            <a class="landscape2 col-xs-4" href="/landscapes/sunset.php">
-                <img src="/images/5x7/Sunset.jpg" class="img-responsive" alt="Silouhette Trees, Sunset, Wasatch Mountain, Rocky Mountain Photograph, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/wasatch-night.php">
+                <img src="../images/5x7/Wasatch-Night.jpg" class="img-responsive" alt="Fireweed, Selective color, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                <img src="/images/5x7/Wasatch-Night.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/timpanogos-from-top.php" >
+                <img src="../images/5x7/Timp-North.jpg" class="img-responsive" alt="Mount Timapanogos, Lake, Summit, Josh Colbert, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" href="/landscapes/lake-blanche.php">
-                <img src="/images/5x7/Sundial-Blanche.jpg" class="img-responsive" alt="Lake Blanche, Sundial Peak, Mount Superior, Monte Cristo, Mountain Lake, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" data-toggle="collapse" href="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                <img src="../images/5x7/Bird-Of-Paridise.jpg" class="img-responsive" alt="Cherry Tree, Wasatch Mountain Range, Utah Landscape Photography"><br>
             </a>
 
 
 
-            <a class="landscape2 col-xs-4" href="/landscapes/downtown-slc.php">
-                <img src="/images/5x7/Downtown-SLC.jpg" class="img-responsive" alt="Downtown Salt Lake City, Salt Lake Temple, Devil Clouds, City Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/sunset.php">
+                <img src="../images/5x7/Sunset.jpg" class="img-responsive" alt="Silouhette Trees, Sunset, Wasatch Mountain, Rocky Mountain Photograph, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                <img src="/images/5x7/Buffalo.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            <a class="landscape2 col-xs-6 col-md-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <img src="../images/5x7/Wasatch-Night.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
             </a>
-            <a class="landscape2 col-xs-4" href="/landscapes/seaweed.php">
-                <img src="/images/5x7/Seaweed.jpg" class="img-responsive" alt="Ocean Trash, Seaweed, California Beach, Beach Photograph"><br>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/lake-blanche.php">
+                <img src="../images/5x7/Sundial-Blanche.jpg" class="img-responsive" alt="Lake Blanche, Sundial Peak, Mount Superior, Monte Cristo, Mountain Lake, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+
+
+
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/downtown-slc.php">
+                <img src="../images/5x7/Downtown-SLC.jpg" class="img-responsive" alt="Downtown Salt Lake City, Salt Lake Temple, Devil Clouds, City Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-6 col-md-4" data-toggle="collapse" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <img src="../images/5x7/Buffalo.jpg" class="img-responsive" alt="Sundial Peak, Wasatch Mountain Range, Utah Landscape Photography"><br>
+            </a>
+            <a class="landscape2 col-xs-6 col-md-4" href="/landscapes/seaweed.php">
+                <img src="../images/5x7/Seaweed.jpg" class="img-responsive" alt="Ocean Trash, Seaweed, California Beach, Beach Photograph"><br>
             </a>
         </div>
+
+
+
+
         <?php include'../templates/footer.php';?>
     </body>
 </html>
