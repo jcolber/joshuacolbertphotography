ef9b3505ba0741153f4946845dfa7c9bee6aeccd
jcolber <jcolber@gmail.com>
2016-06-14 14:12:05 -0600

    Add files via upload

--- templates/footer.php
+++ templates/footer.php
@@ -7,7 +7,9 @@
             <a href="https://www.instagram.com/jcolber/" target="blank"><img src="/images/Social/Instagram.png"  class="img-responsive col-xs-2 pull-right" ></a>
             <a href="https://www.pinterest.com/joshua_colbert/" target="blank"><img src="/images/Social/Pinterest.png" class="img-responsive col-xs-2 pull-right" ></a>
             <a href="https://www.facebook.com/ColbertPhotography.me/" target="blank"><img src="/images/Social/Facebook.png" class="img-responsive col-xs-2  pull-right" ></a>
-            <h5>Copyright © 2012<script>new Date().getFullYear()>2010&&document.write("-"+new Date().getFullYear());</script> Joshua Colbert Photography</h5>
+            <h5>Copyright © 2012<script>new Date().getFullYear()>2010&&document.write("-"+new Date().getFullYear());</script> 
+            <span itemprop="creator" itemscope itemtype="http://schema.org/Person">
+            <span itemprop="name">Joshua Colbert</span> </span>Photography</h5>
         </div>
 
 
