6e63ec1dcdbb8a767269d1f32d0b6b0ac0f6ac8b
Josh Colbert <jcolber@gmail.com>
2017-02-15 22:55:58 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
         
         <title>Joshua Colbert Photography</title>
 
+        <link rel="stylesheet" type="text/css" href="/css/nivo-lightbox/nivo-lightbox.css">
+        <link rel="stylesheet" type="text/css" href="/css/nivo-lightbox/default.css">
     </head>
     
     <body>
         <?php include'templates/navbar.php';?>
-        
-        
-
+        <div class="main"><p>I'm working on this page, but feel free to click on some of the other pages in the meantime. I have a few finished except for this super important landing page. </p>
+        </div>
     </body>
 </html>
